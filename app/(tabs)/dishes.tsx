5fde0896bad1a10968c9a4e8c882adf808eb462e
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-21 20:34:57 +0100

    added. navigation from dishes to recipes. added style

--- app/(tabs)/dishes.tsx
+++ app/(tabs)/dishes.tsx
@@ -3,7 +3,7 @@ import { DISHES } from "@/data/dishes";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function Dishes() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f957d8ff" }}>
       <DishesList dishes={DISHES} />
     </SafeAreaView>
   );
--- components/my/DishsList.tsx
+++ components/my/DishsList.tsx
@@ -1,5 +1,6 @@
 import { useNavigateContext } from "@/contexts/NavigateContext";
-import { FlatList } from "react-native";
+import { useRouter } from "expo-router";
+import { FlatList, Pressable } from "react-native";
 import DishCard from "./DishCard";
 
 type Dish = {
@@ -14,6 +15,7 @@ type DishesProps = {
   dishes: Dish[];
 };
 export default function DishesList({ dishes }: DishesProps) {
+  const router = useRouter();
   const { categoryId } = useNavigateContext();
   const dishesToRender = dishes.filter((d) => d.categoryId === categoryId);
   return (
@@ -21,11 +23,16 @@ export default function DishesList({ dishes }: DishesProps) {
       data={dishesToRender}
       keyExtractor={(item) => item.id.toString()}
       renderItem={(itemData) => (
-        <DishCard
-          imageUrl={itemData.item.image}
-          nameDish={itemData.item.name}
-          description={itemData.item.description}
-        />
+        <Pressable
+          onPress={() => router.push("/recipes")}
+          style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }]}
+        >
+          <DishCard
+            imageUrl={itemData.item.image}
+            nameDish={itemData.item.name}
+            description={itemData.item.description}
+          />
+        </Pressable>
       )}
     />
   );
